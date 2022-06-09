136f432f46b9ed614157a2be7e714e9fcb1d9ea4
Borodur <matt.sol@o2.pl>
2022-06-09 21:54:22 +0200

    Zapomniałem wrzucić tego wcześniej

---
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab4/Kulka.java
@@ -0,0 +1,56 @@
+package pl.lublin.wsei.java.cwiczenia.lab4;
+
+import java.util.Random;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+public class Kulka {
+
+    private static final double R = 10;
+    protected double ySpeed;
+    protected double xSpeed;
+    protected double xPos;
+    protected double yPos;
+    private double radius = 10;
+    protected Color color;
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void setColor(Color color){
+        this.color = color;
+    }
+
+    public double getRadius(){
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    Kulka(double xPos, double yPos, double xSpeed, double ySpeed, Color color){
+        this(xPos, yPos, xSpeed, ySpeed);
+        this.color = color;
+    }
+
+    public void checkBoundaryCollision(double xLeft, double yTop, double xRight, double yBottom){
+        if ((xPos - radius <= xLeft) || ((xPos + radius >= xRight))){
+            xSpeed=-xSpeed;
+        }
+        if ((yPos - radius <= yTop) || ((yPos + radius >= yBottom))){
+            ySpeed=-ySpeed;
+        }
+    }
+
+    public void draw(GraphicsContext gc){
+        gc.setFill(color);
+        gc.fillOval(xPos - radius, yPos - radius, 2*radius, 2*radius);
+    }
+
+    public void update(){
+        xPos += xSpeed;
+        yPos += ySpeed;
+    }
+}
